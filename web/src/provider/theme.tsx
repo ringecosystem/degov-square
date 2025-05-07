ac013435dda9acb77b1737c5127564f9477017e4
snoopy1412 <snoopy1412.su@gmail.com>
2025-05-07 22:50:42 +0800

    Initialize web project

---
+++ web/src/provider/theme.tsx
@@ -0,0 +1,9 @@
+import { ThemeProvider } from "next-themes";
+
+export function NextThemeProvider({ children }: { children: React.ReactNode }) {
+  return (
+    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+      {children}
+    </ThemeProvider>
+  );
+}
