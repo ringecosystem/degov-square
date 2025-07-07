64081c71ccb3708456877de4ecda9b3261e694ef
fewensa <osuni@protonmail.com>
2025-07-07 07:38:32 +0000

    fix(server): update GraphQL playground handler and log message for better clarity

--- backend/server.go
+++ backend/server.go
@@ -35,9 +35,10 @@ func main() {
 		Cache: lru.New[string](100),
 	})
 
-	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	graphiql := playground.Handler("GraphQL playground", "/query")
+	http.Handle("/", graphiql)
 	http.Handle("/query", srv)
 
-	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
+	log.Printf("connect to http://::%s/ for GraphQL playground", port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
