ef37561566bdd6118a5b62191b873904b2328a31
fewensa <osuni@protonmail.com>
2025-08-12 04:51:00 +0000

    Remove unused Detect handler from DaoRoute and clean up imports

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -147,7 +147,6 @@ func startServer() {
 	// Support both patterns: /dao/config and /dao/config/{dao}
 	mux.Handle("/dao/config", middlewareChain.Then(http.HandlerFunc(daoRoute.ConfigHandler)))
 	mux.Handle("/dao/config/{dao}", middlewareChain.Then(http.HandlerFunc(daoRoute.ConfigHandler)))
-	mux.Handle("/dao/detect", middlewareChain.Then(http.HandlerFunc(daoRoute.Detect)))
 
 	corsHandler := cors.New(cors.Options{
 		AllowedOrigins:   []string{"*"},
