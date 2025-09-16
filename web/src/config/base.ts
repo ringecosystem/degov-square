ba234e9b7ffb0b58ae016720c38e273bdf1a3443
fewensa <osuni@protonmail.com>
2025-09-16 08:41:32 +0000

    Fix app name in configuration from 'Apps' to 'Square'

--- web/src/config/base.ts
+++ web/src/config/base.ts
@@ -3,6 +3,6 @@ export const DEFAULT_CONFIRMATIONS = 5;
 export const DEFAULT_REFETCH_INTERVAL = 10_000;
 export const DEFAULT_PAGE_SIZE = 10;
 export const DEFAULT_ANIMATION_DURATION = 0.1;
-export const APP_NAME = 'Apps | DeGov.AI';
+export const APP_NAME = 'Square | DeGov.AI';
 export const APP_DESCRIPTION =
   'DeGov.AI is a AI-powered platform for decentralized governance, built on the Openzeppelin contracts.';
