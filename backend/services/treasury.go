f9da3d069a2b2ef749d4bc8cba5d8474b98430e0
fewensa <osuni@protonmail.com>
2025-10-17 07:27:54 +0000

    Refactor LoadTreasuryAssets to conditionally fetch historical prices based on balance

--- backend/services/treasury.go
+++ backend/services/treasury.go
@@ -1,6 +1,7 @@
 package services
 
 import (
+	"strconv"
 	"time"
 
 	"github.com/patrickmn/go-cache"
@@ -69,21 +70,24 @@ func (s *TreasuryService) LoadTreasuryAssets(input *gqlmodels.TreasuryAssetsInpu
 		}
 
 		var treasuryHistoricalPrices []*gqlmodels.TreasuryHistoricalPrice
-		histories, err := s.okx.HistoricalPrice(internal.OkxHistoricalPriceOptions{
-			Chain:   input.Chain,
-			Address: firstPlatform.Address,
-			Limit:   2,
-			Begin:   utils.Int64Ptr(now.Add(-2 * 24 * time.Hour).UnixMilli()), // 2 days ago
-			// End:     utils.Int64Ptr(now.UnixMilli()),
-			// Period:  internal.OkxPeriod1d,
-		})
-		if err == nil && len(histories) > 0 {
-			history := histories[0]
-			for _, hp := range history.Prices {
-				treasuryHistoricalPrices = append(treasuryHistoricalPrices, &gqlmodels.TreasuryHistoricalPrice{
-					Timestamp: hp.Time,
-					Price:     hp.Price,
-				})
+		balanceUsdFloat, err := strconv.ParseFloat(firstPlatform.BalanceUSD, 64)
+		if err != nil && balanceUsdFloat >= 0.01 {
+			histories, err := s.okx.HistoricalPrice(internal.OkxHistoricalPriceOptions{
+				Chain:   input.Chain,
+				Address: firstPlatform.Address,
+				Limit:   2,
+				Begin:   utils.Int64Ptr(now.Add(-2 * 24 * time.Hour).UnixMilli()), // 2 days ago
+				// End:     utils.Int64Ptr(now.UnixMilli()),
+				// Period:  internal.OkxPeriod1d,
+			})
+			if err == nil && len(histories) > 0 {
+				history := histories[0]
+				for _, hp := range history.Prices {
+					treasuryHistoricalPrices = append(treasuryHistoricalPrices, &gqlmodels.TreasuryHistoricalPrice{
+						Timestamp: hp.Time,
+						Price:     hp.Price,
+					})
+				}
 			}
 		}
 
